80d916b8c9d3e6f3839af174c7818be91ccbaf8a
vChavezB <47216966+vChavezB@users.noreply.github.com>
2023-07-25 14:02:11 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 set -x
 set -e
 
+# Move to west workdir
 cd /workdir/project
 mkdir build_periph
 mkdir build_central
+# build the peripheral hr sample
 west build --board nrf52840dk_nrf52840 --build-dir build_periph $ZEPHYR_BASE/samples/bluetooth/peripheral_hr
+# build the central hr sample
 west build --board nrf52840dk_nrf52840 --build-dir build_central $ZEPHYR_BASE/samples/bluetooth/central_hr
+
+# move build files to artifacts dir
+mv build_periph/zephyr/zephyr.elf $GITHUB_WORKSPACE/artifacts/periph.elf
+mv build_central/zephyr/zephyr.elf $GITHUB_WORKSPACE/artifacts/central.elf
