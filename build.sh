0462ba7356868d764e2e33475c47fda2e7665292
vChavezB <47216966+vChavezB@users.noreply.github.com>
2023-07-25 13:55:38 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 set -x
 set -e
 
-cd $ZEPHYR_BASE
+cd /workdir/project
 mkdir build_periph
 mkdir build_central
-west build --board nrf52840dk_nrf5240 --build-dir build_periph zephyr/samples/bluetooth/peripheral_hr
-west build --board nrf52840dk_nrf5240 --build-dir build_central zephyr/samples/bluetooth/central_hr
+west build --board nrf52840dk_nrf5240 --build-dir build_periph $ZEPHYR_BASE/samples/bluetooth/peripheral_hr
+west build --board nrf52840dk_nrf5240 --build-dir build_central $ZEPHYR_BASE/samples/bluetooth/central_hr
