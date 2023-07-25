5f987125d20322ce286be2655d69e97653eaf51e
vChavezB <47216966+vChavezB@users.noreply.github.com>
2023-07-25 13:52:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 set -x
 set -e
 
-echo "This is my build artifact" > artifacts/build-result
+cd $ZEPHYR_BASE
+mkdir build_periph
+mkdir build_central
+west build --board nrf52840dk_nrf5240 --build-dir build_periph zephyr/samples/bluetooth/peripheral_hr
+west build --board nrf52840dk_nrf5240 --build-dir build_central zephyr/samples/bluetooth/central_hr
