cee3f77967dc99c39c9a472b80eb63458917233b
Victor Chavez <47216966+vChavezB@users.noreply.github.com>
2024-05-28 20:01:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ mkdir build_central
 chmod +x /opt/toolchains/zephyr-sdk-${ZSDK_VERSION}/setup.sh
 .opt/toolchains/zephyr-sdk-${ZSDK_VERSION}/setup.sh
 # build the peripheral hr sample
-west build --board nrf52840dk_nrf52840 --build-dir build_periph /tmp/zephyr/samples/bluetooth/peripheral_hr
+west build --board nrf52840dk_nrf52840 --build-dir build_periph $GITHUB_WORKSPACE/../zephyr/samples/bluetooth/peripheral_hr
 # build the central hr sample
-west build --board nrf52840dk_nrf52840 --build-dir build_central /tmp/zephyr/samples/bluetooth/central_hr
+west build --board nrf52840dk_nrf52840 --build-dir build_central $GITHUB_WORKSPACE/../zephyr/samples/bluetooth/central_hr
 
 # move build files to artifacts dir
 mv build_periph/zephyr/zephyr.elf $GITHUB_WORKSPACE/artifacts/periph.elf
